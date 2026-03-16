fd97a3c0fab2f922a766dbb0b98919f95892fc90
sohancreation <sohancreation@users.noreply.github.com>
2026-03-16 23:56:20 +0600

    Configure GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename="/bloodlink">
             <AuthProvider>
               <Routes>
                 <Route path="/" element={<Landing />} />
