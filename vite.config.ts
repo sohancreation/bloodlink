e069a2c20fc63659c49a02c1b64f079e26cfda10
sohancreation <sohancreation@users.noreply.github.com>
2026-03-19 14:44:43 +0600

    Configure deployment for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/bloodlink/" : "/",
+  base: mode === "production" ? (process.env.NETLIFY === "true" ? "/" : "/bloodlink/") : "/",
   server: {
     host: "::",
     port: 8080,
